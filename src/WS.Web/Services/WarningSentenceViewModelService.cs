dc26646acd5c61eb6106273c60ffeb5f5c86246c
FrederikBA <frederikbandersen98@gmail.com>
2024-04-18 14:36:02 +0200

    map to viewmodel

--- src/WS.Web/Services/WarningSentenceViewModelService.cs
+++ src/WS.Web/Services/WarningSentenceViewModelService.cs
@@ -56,4 +56,48 @@ public class WarningSentenceViewModelService : IWarningSentenceViewModelService
 
         return warningSentenceViewModels;
     }
+
+    public async Task<WarningSentenceViewModel> GetWarningSentenceViewModel(int id)
+    {
+        var warningSentenceEntity = await _warningSentenceService.GetWarningSentenceByIdAsync(id);
+        
+        var warningSentenceViewModel = new WarningSentenceViewModel
+        {
+            Id = warningSentenceEntity.Id,
+            Code = warningSentenceEntity.Code,
+            Text = warningSentenceEntity.Text,
+            
+            //Warning Category
+            WarningCategory = new WarningCategoryViewModel
+            {
+                Id = warningSentenceEntity.WarningCategory!.Id,
+                Text = warningSentenceEntity.WarningCategory!.Text,
+                
+                //Warning Type
+                WarningType = new WarningTypeViewModel
+                {
+                    Type = warningSentenceEntity.WarningCategory!.WarningType!.Type,
+                }
+            },
+            
+            //Warning Pictogram
+            WarningPictogram = new WarningPictogramViewModel
+            {
+                Id = warningSentenceEntity.WarningPictogram!.Id,
+                Code = warningSentenceEntity.WarningPictogram!.Code,
+                Text = warningSentenceEntity.WarningPictogram!.Text,
+                Pictogram = warningSentenceEntity.WarningPictogram!.Pictogram,
+                Extension = warningSentenceEntity.WarningPictogram!.Extension,
+            },
+            
+            //Warning Signal Word
+            WarningSignalWord = new WarningSignalWordViewModel
+            {
+                Id = warningSentenceEntity.WarningSignalWord!.Id,
+                SignalWordText = warningSentenceEntity.WarningSignalWord!.SignalWordText,
+            }
+        };
+        
+        return warningSentenceViewModel;
+    }
 }
